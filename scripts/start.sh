3146493603d37d2cc92f9a5fc56430372a041b1d
Cristian Georgiu <cristi.georgiu@gmail.com>
2022-10-28 12:54:24 +0300

    simplified runner removal

--- scripts/start.sh
+++ scripts/start.sh
@@ -15,7 +15,7 @@ cd /home/docker/actions-runner
 
 cleanup() {
     echo "Removing runner..."
-    ./config.sh remove --unattended --token ${REG_TOKEN}
+    ./config.sh remove
 }
 
 trap 'cleanup; exit 130' INT
